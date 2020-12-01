ae7b8082e13c4819ac44dcb74be80c0788e6b008
hasanqazi <hasanahmedhaf@gmail.com>
2020-12-01 17:22:13 -0600

    fixed return statements

--- day1/main.py
+++ day1/main.py
@@ -9,7 +9,7 @@ def find_sum():
     for j in ints:
       if (i+j) == 2020:
         print(i*j)
-        return
+        break
 
 def find_sum2():
   for i in ints:
@@ -17,7 +17,7 @@ def find_sum2():
       for k in ints:
         if (i+j+k) == 2020:
           print(i*j*k)
-          return
+          break
 
 find_sum()
 find_sum2()
