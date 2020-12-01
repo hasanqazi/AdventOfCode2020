380191548014bee4686af6b4073d82fcb0906543
hasanqazi <hasanahmedhaf@gmail.com>
2020-12-01 06:57:01 -0600

    initial commit

---
+++ day1/main.py
@@ -0,0 +1,23 @@
+ints=[]
+
+with open("input") as f:
+  for i in f:
+    ints.append(int(i))
+
+def find_sum():
+  for i in ints:
+    for j in ints:
+      if (i+j) == 2020:
+        print(i*j)
+        return
+
+def find_sum2():
+  for i in ints:
+    for j in ints:
+      for k in ints:
+        if (i+j+k) == 2020:
+          print(i*j*k)
+          return
+
+find_sum()
+find_sum2()
