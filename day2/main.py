e1f96e8928d889e2ccf00bbfa791d2165a94fb07
hasanqazi <hasanahmedhaf@gmail.com>
2020-12-02 19:12:46 -0600

    solution for day 2 part 2 completed

--- day2/main.py
+++ day2/main.py
@@ -8,22 +8,38 @@ def check_pass():
   valid_pass = 0
   check = 0
   for i in range(len(input)):
-    first_limit = input[i].split('-')[0]
-    second_limit = str(input[i].split('-')[1]).split(' ')[0]
+    first_limit = int(input[i].split('-')[0])
+    second_limit = int(str(input[i].split('-')[1]).split(' ')[0])
     char = str(input[i].split(':')[0])[-1]
     password = input[i].split()[2]
-    #print(first_limit, second_limit, char, password)
     for j in range(len(password)):
       if password[j] == char:
         check += 1
-    if check >= int(first_limit) and check <= int(second_limit):
+    if check >= first_limit and check <= second_limit:
       print("Valid password")
-      #print(first_limit, second_limit, check)
       check = 0
       valid_pass += 1
     else:
       print("Invalid password")
-      #print(first_limit, second_limit, check)
       check = 0
-  print(valid_pass)
+  print("Valid passwords: ", valid_pass)
+
+def check_pass2():
+  valid_pass = 0
+  for i in range(len(input)):
+    first_char = int(input[i].split('-')[0])-1
+    second_char = int(str(input[i].split('-')[1]).split(' ')[0])-1
+    char = str(input[i].split(':')[0])[-1]
+    password = input[i].split()[2]
+    if password[first_char] == char and password[second_char] != char:
+      print("Valid password")
+      valid_pass += 1
+    elif password[first_char] != char and password[second_char] == char:
+      print("Valid password")
+      valid_pass += 1
+    else:
+      print("Invalid password")
+  print("Valid passwords: ", valid_pass)
+  
 check_pass()
+check_pass2()
